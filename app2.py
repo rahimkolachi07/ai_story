0081646bad86b7a20c32e6aa5b37b30845b7e7fb
rahimkolachi07 <raheemkolachi7@gmail.com>
2024-03-24 17:36:01 +0500

    update

--- app2.py
+++ app2.py
@@ -1,13 +1,15 @@
-from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
-import torch
+from diffusers import StableCascadeCombinedPipeline
 
-model_id = "stabilityai/stable-diffusion-2-1-base"
+pipe = StableCascadeCombinedPipeline.from_pretrained("stabilityai/stable-cascade", variant="bf16", torch_dtype=torch.bfloat16)
 
-scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
-pipe = pipe.to("cuda")
-
-prompt = "a photo of an astronaut riding a horse on mars"
-image = pipe(prompt).images[0]  
-    
-image.save("astronaut_rides_horse.png")
+prompt = "an image of a shiba inu, donning a spacesuit and helmet"
+output = pipe(
+    prompt=prompt,
+    negative_prompt="",
+    num_inference_steps=10,
+    prior_num_inference_steps=20,
+    prior_guidance_scale=3.0,
+    width=1024,
+    height=1024,
+)
+output.images[0].save("cascade-combined.png")
